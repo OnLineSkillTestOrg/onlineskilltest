6f55ddeaa15346e718d7417525282c36da6797cf
Anumodh <anumodh.sharma@gmail.com>
2016-03-22 11:45:28 +0530

    Added first Git Commit

--- TestManager/src/main/java/com/nab/skilltest/service/ExamService.java
+++ TestManager/src/main/java/com/nab/skilltest/service/ExamService.java
@@ -1,8 +1,26 @@
 package com.nab.skilltest.service;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.nab.skilltest.model.User;
+
+
 @Service
-public class ExamService {
+public interface ExamService {
+	
+	
+	@POST
+	@Path("/exam")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	ResponseEntity<User> createExam(@PathParam("templateId") String templateId);
 
 }
